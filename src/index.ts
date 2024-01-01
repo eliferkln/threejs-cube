79e9135cd0283d0f7800389aafb2251d30f41f9a
Hanife Erkalan <73136159+eliferkln@users.noreply.github.com>
2024-01-01 16:53:14 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -11,15 +11,22 @@ camera.position.z = 5;
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+const light = new THREE.PointLight(0xff0000, 5, 100);
+light.position.set(0, 0, 10);
+scene.add(light);
+
 const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ffba8 });
+const material = new THREE.MeshStandardMaterial({ color: 0x00ffba8 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
+
 const animate = () => {
+
   requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.06;
+  cube.rotation.x += 0.5;
+  cube.rotation.y += 0.5;
   renderer.render(scene, camera);
 };
 
